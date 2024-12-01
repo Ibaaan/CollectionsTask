c094d368f8bf2d73c52df02acb80b5d7418c0d3f
Ivan <kuuuuushtanov@gmail.com>
2024-12-01 16:13:39 +0500

    Task 1 + Task 2

--- src/main/java/ru/naumen/collection/task2/Task2.java
+++ src/main/java/ru/naumen/collection/task2/Task2.java
@@ -1,7 +1,6 @@
 package ru.naumen.collection.task2;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Дано:
@@ -31,12 +30,23 @@ import java.util.List;
  */
 public class Task2
 {
-
     /**
      * Возвращает дубликаты пользователей, которые есть в обеих коллекциях
      */
     public static List<User> findDuplicates(Collection<User> collA, Collection<User> collB) {
-        // TODO реализовать метод
-        return null;
+        Set<User> set = new HashSet<>(collA);
+        List<User> result = new ArrayList<>();
+        for(User user : collB){
+            if(set.contains(user)){
+                result.add(user);
+            }
+        }
+        return result;
     }
 }
+/*
+Сложность - О(n+m), где O(n) - создание HashSet из коллекции collA,
+                        O(m) - итерирование коллекции collB
+Почему - Set позволяет хранить множество неповторяющихся элементов,
+         HashSet позволяет добавить элемент и узнать принадлежит ли элемент коллекции за O(1)
+ */
--- src/main/java/ru/naumen/collection/task2/User.java
+++ src/main/java/ru/naumen/collection/task2/User.java
@@ -1,5 +1,8 @@
 package ru.naumen.collection.task2;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Пользователь
  *
@@ -10,4 +13,36 @@ public class User {
     private String username;
     private String email;
     private byte[] passwordHash;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail(){return email;}
+
+    public byte[] getPasswordHash(){return passwordHash;}
+
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj){
+            return true;
+        }
+        if(!(obj instanceof User other)) {
+            return false;
+        }
+        if(other.hashCode() == this.hashCode()){
+            return Objects.equals(this.username, other.getUsername()) &&
+                    Objects.equals(this.email, other.getEmail()) &&
+                    Arrays.equals(this.passwordHash, other.getPasswordHash());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode(){
+        int hash = username.hashCode() * 31;
+        hash += email.hashCode() * 31;
+        hash += Arrays.hashCode(passwordHash);
+        return hash;
+    }
 }
